b92702c272f89328f3ee62f862ca673df60d1038
Fernando Paredes <nano@fdp.io>
2016-12-28 16:30:30 -0800

    [Functions] Add a fn that returns a number

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,15 @@ fn print_number(x: i32) {
     println!("x is: {}", x);
 }
 
+fn add_one(n: i32) -> i32 {
+    n + 1
+    // Lack of semicolon on returns
+    // Return is implied on last line, similar to Ruby
+}
+
 fn main() {
     println!("Hello, World!");
+    let x = add_one(12);
+    println!("add_one to 12 is: {}", x);
     print_number(32)
 }
